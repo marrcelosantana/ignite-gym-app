e6a0e036c68ebc543db030354438cc88470a2632
marrcelosantana <marcelosantana@alu.ufc.br>
2023-02-18 18:22:39 -0300

    [Ignite-Gym] - Add Profile Components

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -22,7 +22,7 @@ export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
-      <ScrollView>
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={6} px={10}>
           {photoIsLoading ? (
             <Skeleton
@@ -64,7 +64,7 @@ export function Profile() {
         </Center>
 
         <VStack px={10} mt={12} mb={9}>
-          <Heading color="gray.200" fontSize="md" mb={2}>
+          <Heading color="gray.200" fontSize="md" mb={2} alignSelf="flex-start">
             Alterar senha
           </Heading>
 
