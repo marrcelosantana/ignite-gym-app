b628b7d5fc8eb8fbe86adbaa0c1356fb1ed92f58
marrcelosantana <marcelosantana@alu.ufc.br>
2023-02-18 18:59:06 -0300

    [Ignite-Gym] - Add Exercise Page Components

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -5,6 +5,9 @@ import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
 import { ExerciseCard } from "@components/ExerciseCard";
 
+import { useNavigation } from "@react-navigation/native";
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
+
 export function Home() {
   const [groups, setGrous] = useState(["costas", "biceps", "triceps", "ombro"]);
   const [groupSelected, setGroupSelected] = useState("costas");
@@ -14,6 +17,12 @@ export function Home() {
     "Remada Unilateral",
     "Levantamento Terras",
   ]);
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleOpenExerciseDetails() {
+    navigation.navigate("exercise");
+  }
 
   return (
     <VStack flex={1}>
@@ -36,6 +45,7 @@ export function Home() {
         _contentContainerStyle={{ px: 8 }}
         my={10}
         maxH={10}
+        minH={10}
       />
 
       <VStack flex={1} px={8}>
@@ -51,7 +61,9 @@ export function Home() {
         <FlatList
           data={exercises}
           keyExtractor={(item) => item}
-          renderItem={({ item }) => <ExerciseCard />}
+          renderItem={({ item }) => (
+            <ExerciseCard onPress={handleOpenExerciseDetails} />
+          )}
           showsVerticalScrollIndicator={false}
           _contentContainerStyle={{ paddingBottom: 20 }}
         />
